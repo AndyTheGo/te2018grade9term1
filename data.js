95d812184973c8a108a1fefef2783626290c317b
Mahim Arib <mahim216@gmail.com>
2018-10-06 15:45:38 -0400

    Challenge 1 Completed

--- data.js
+++ data.js
@@ -3,3 +3,15 @@ let message = "Hello World!";
 console.log(message);
 
 // Type your code below!
+let m = 30;
+let d = 2;
+let s = 9;
+let c = "Hello World";
+let yoyo = "I don't want you anymore";
+let zoe = "You disgust me";
+let isARapper = false;
+Let isAKid = true;
+let studentID = undefined;
+let momName = undefined
+let userInput = READLINE.question:("type anything and I'll repeat it back")
+console.log(You typed:(userInput); 
